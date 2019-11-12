9429e6047e3e4281640792408244e02521f97033
Dave Farinelli <d@dfar.io>
2019-11-12 09:30:43 -0500

    Adding https_only

--- main.tf
+++ main.tf
@@ -15,6 +15,7 @@ resource "azurerm_app_service" "as" {
   resource_group_name = "${var.rg_name}"
   app_service_plan_id = "${azurerm_app_service_plan.asp.id}"
   app_settings        = "${var.app_settings}"
+  https_only          = "${var.https_only}"
   site_config {
     always_on = "${var.always_on}"
   }
